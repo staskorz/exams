0324b4e4dd80fab7b7db0ea88b13daa793f6bb9f
staskorz <staskorz@gmail.com>
2016-07-29 12:47:17 +0300

    Reformat code

--- imports/ui/layouts/Layout.jsx
+++ imports/ui/layouts/Layout.jsx
@@ -13,7 +13,7 @@ const styles = {
 	titleIcon: {
 		marginRight: '8px',
 		marginBottom: '2px',
-		verticalAlign: 'text-bottom'		
+		verticalAlign: 'text-bottom'
 	},
 	
 	link: {
@@ -31,17 +31,17 @@ const styles = {
 
 
 export default ({ children }) => (
-	<div>
-		{<AppBar
-			title={ <Link to='/' style={ styles.title }><SchoolIcon color='white' style={ styles.titleIcon } />Exams</Link> }
-			showMenuIconButton={ false }
-			iconElementRight={ <span>
+		<div>
+			<AppBar
+					title={ <Link to='/' style={ styles.title }><SchoolIcon color='white' style={ styles.titleIcon } />Exams</Link> }
+					showMenuIconButton={ false }
+					iconElementRight={ <span>
 				<Link to='/' style={ styles.link } activeStyle={ styles.linkActive } onlyActiveOnIndex={ true }>Home</Link>
 				<Link to='/page1' style={ styles.link } activeStyle={ styles.linkActive }>Page1</Link>
 				<Link to='/create-exam' style={ styles.link } activeStyle={ styles.linkActive }>Create Exam</Link>
 			</span> }
-		/>}
-		
-		{ children }
-	</div>
+			/>
+			
+			{ children }
+		</div>
 );
