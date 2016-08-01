75bc19e7b4c6fc02cf8405a4681a4743fbe2717f
staskorz <staskorz@gmail.com>
2016-08-01 13:44:30 +0300

    Reformat code

--- imports/ui/pages/CreateExam.jsx
+++ imports/ui/pages/CreateExam.jsx
@@ -14,18 +14,18 @@ export default class CreateExam extends Component {
 	
 	render() {
 		return (
-			<Card style={ this.style.card }>
-				<CardTitle title='Create Exam' />
-				<CardText>
-					<form>
-						<TextField name='name' hintText='Exam Name' /><br />
-						<TextField name='number' type='number' hintText='Exam Number' />
-					</form>
-				</CardText>
-				<CardActions>
-					<FlatButton label='Save' primary={ true } /><FlatButton label='Cancel' />
-				</CardActions>
-			</Card>
+				<Card style={ this.style.card }>
+					<CardTitle title='Create Exam' />
+					<CardText>
+						<form>
+							<TextField name='name' hintText='Exam Name' /><br />
+							<TextField name='number' type='number' hintText='Exam Number' />
+						</form>
+					</CardText>
+					<CardActions>
+						<FlatButton label='Save' primary={ true } /><FlatButton label='Cancel' />
+					</CardActions>
+				</Card>
 		);
 	}
 }
