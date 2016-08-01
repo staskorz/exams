a896171bf280a3fcd34ffee663af6c4d40e743a0
staskorz <staskorz@gmail.com>
2016-08-01 20:32:02 +0300

    WIP: add basic question edit component

---
+++ imports/ui/components/QuestionEdit.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Paper, Badge, TextField, Checkbox, FloatingActionButton } from 'material-ui';
+import IconRemove from 'material-ui/svg-icons/content/remove';
+
+
+export default class QuestionEdit extends Component {
+	state = {
+		numOfAnswers: 1,
+		multipleAnswers: false
+	};
+	
+	style = {
+		paper: {
+			marginTop: '40px',
+			paddingTop: '10px',
+			position: 'relative'
+		},
+		
+		fieldsContainer: {
+			padding: '40px',
+			paddingTop: '0px'
+		},
+		
+		numOfAnswersLabel: {
+			color: '#000000de'
+		},
+		
+		numOfAnswers: {
+			width: '25px'
+		},
+		
+		multipleAnswersCheckbox: {
+			width: '174px'
+		},
+		
+		removeQuestionButton: {
+			position: 'absolute',
+			bottom: '10px',
+			right: '10px'
+		}
+	};
+	
+	
+	handleQuestionRemoveButtonClick = () => {
+		this.props.onRemove(this.props.questionNumber);
+	};
+	
+	
+	render() {
+		return (
+				<Paper style={ this.style.paper }>
+					<Badge badgeContent={ this.props.questionNumber } primary={ true } />
+					
+					<div style={ this.style.fieldsContainer }>
+						<TextField name='text'
+								   multiLine={ true } rows={ 1 } rowsMax={ 7 } fullWidth
+								   floatingLabelText='Question Body' hintText='Question Body' /><br />
+						
+						<span style={ this.style.numOfAnswersLabel }>Number Of Answers </span>
+						<TextField name='numOfAnswers'
+								   type='number' defaultValue={ 4 }
+								   style={ this.style.numOfAnswers } />
+						
+						<Checkbox style={ this.style.multipleAnswersCheckbox } label='Multiple Answers' labelPosition='left' />
+					</div>
+					<FloatingActionButton mini={true} style={ this.style.removeQuestionButton } onTouchTap={ this.handleQuestionRemoveButtonClick }>
+						<IconRemove />
+					</FloatingActionButton>
+				</Paper>
+		);
+	}
+}
--- imports/ui/pages/CreateExam.jsx
+++ imports/ui/pages/CreateExam.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, CardText, CardActions, TextField, FlatButton } from 'material-ui';
+import { Card, CardTitle, CardText, CardActions, TextField, FlatButton, FloatingActionButton } from 'material-ui';
+import IconAdd from 'material-ui/svg-icons/content/add';
+
+import QuestionEdit from '/imports/ui/components/QuestionEdit'
 
 
 export default class CreateExam extends Component {
@@ -8,6 +11,11 @@ export default class CreateExam extends Component {
 			margin: '80px',
 			marginTop: '40px',
 			padding: '90px'
+		},
+		
+		addQuestionButton: {
+			marginLeft: '10px',
+			marginTop: '10px'
 		}
 	};
 	
@@ -21,6 +29,9 @@ export default class CreateExam extends Component {
 							<TextField name='name' hintText='Exam Name' /><br />
 							<TextField name='number' type='number' hintText='Exam Number' />
 						</form>
+						
+						<QuestionEdit questionNumber={ 1 } />
+						<FloatingActionButton mini={true} style={ this.style.addQuestionButton }><IconAdd /></FloatingActionButton>
 					</CardText>
 					<CardActions>
 						<FlatButton label='Save' primary={ true } /><FlatButton label='Cancel' />
