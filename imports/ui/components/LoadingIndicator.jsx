38c8c500f585e79c079b53bca05c56cef53dfa9b
staskorz <staskorz@gmail.com>
2016-08-23 11:14:09 +0300

    Center the loading indicator

--- imports/ui/components/LoadingIndicator.jsx
+++ imports/ui/components/LoadingIndicator.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { CircularProgress } from 'material-ui';
 
 
+const style = {
+	margin: 'auto',
+	position: 'absolute',
+	top: 0,
+	left: 0,
+	bottom: 0,
+	right: 0
+};
+
+
 export default () => (
-		<CircularProgress size={ 1.5 } />
+		<CircularProgress size={ 1.5 } style={ style } />
 );
