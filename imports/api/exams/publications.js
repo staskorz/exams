d032dff539400bc8c085fe6803620e18c77fdf0d
staskorz <staskorz@gmail.com>
2016-08-30 16:41:21 +0300

    Sort exams by name

--- imports/api/exams/publications.js
+++ imports/api/exams/publications.js
@@ -4,4 +4,4 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import collection from './collection';
 
 
-Meteor.publish('exams', () => collection.find());
+Meteor.publish('exams', () => collection.find({}, { sort: { name: 1 } }));
