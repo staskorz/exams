b67ac7125d50086e14c5e936bc13aa7e57a68c66
staskorz <staskorz@gmail.com>
2016-08-31 13:39:08 +0300

    Countdown seconds for reconnection retrying

--- imports/ui/components/ConnectionStatus.jsx
+++ imports/ui/components/ConnectionStatus.jsx
@@ -1,43 +1,108 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Paper, FlatButton } from 'material-ui';
 import { pinkA200 } from 'material-ui/styles/colors'
 
 
-const style = {
-	mainContainer: {
-		display: 'inline-block',
-		marginRight: '20px',
-		backgroundColor: pinkA200,
-		color: 'white'
-	},
+export default class ConnectionStatus extends Component {
+	state = {
+		retryingIn: null,
+		intervalId: null
+	};
 	
-	message: {
-		paddingLeft: '16px'
-	},
 	
-	buttonLabel: {
-		color: 'white',
-		textDecoration: 'underline'
-	}
-};
-
-export default ({ connected, retryingIn, reconnect }) => {
-	if(connected) {
-		return <div></div>;
-	} else {
-		let message;
+	style = {
+		mainContainer: {
+			display: 'inline-block',
+			marginRight: '20px',
+			backgroundColor: pinkA200,
+			color: 'white'
+		},
 		
-		if(retryingIn) {
-			message = 'Retrying in ' + retryingIn + 's.';
-		} else {
-			message = 'Disconnected.';
+		message: {
+			paddingLeft: '16px'
+		},
+		
+		buttonLabel: {
+			color: 'white',
+			textDecoration: 'underline'
 		}
+	};
+	
+	
+	clearRunningInterval = () => {
+		if(this.state.intervalId) {
+			clearInterval(this.state.intervalId);
+		}
+	};
+	
+	
+	respondToRetryingInUpdate = (retryingIn) => {
+		if(retryingIn) {
+			this.clearRunningInterval();
+			
+			this.setState({
+				intervalId: setInterval(() => {
+					const { retryingIn } = this.state;
+					
+					if(retryingIn) {
+						this.setState({
+							retryingIn: retryingIn - 1
+						});
+					} else {
+						this.clearRunningInterval();
+						
+						this.setState({
+							intervalId: null
+						});
+					}
+				}, 1000),
+				
+				retryingIn
+			});
+		} else {
+			if(this.state.intervalId) {
+				this.clearRunningInterval();
+				
+				this.setState({
+					intervalId: null
+				});
+			}
+		}
+	};
+	
+	
+	componentDidMount() {
+		this.respondToRetryingInUpdate(this.props.retryingIn);
+	};
+	
+	
+	componentWillReceiveProps(nextProps) {
+		this.respondToRetryingInUpdate(nextProps.retryingIn);
+	};
+	
+	
+	render() {
+		const { connected, reconnect } = this.props;
 		
-		return (
-				<Paper style={ style.mainContainer }>
-					<span style={ style.message }>{ message }</span>
-					<FlatButton label={ 'Reconnect Now' } labelStyle={ style.buttonLabel } onClick={ reconnect } />
-				</Paper>
-		);
-	}
+		const { retryingIn } = this.state;
+		
+		if(connected) {
+			return <div></div>;
+		} else {
+			let message;
+			
+			if(retryingIn) {
+				message = 'Retrying in ' + retryingIn + 's.';
+			} else {
+				message = 'Disconnected.';
+			}
+			
+			return (
+					<Paper style={ this.style.mainContainer }>
+						<span style={ this.style.message }>{ message }</span>
+						<FlatButton label={ 'Reconnect Now' } labelStyle={ this.style.buttonLabel } onClick={ reconnect } />
+					</Paper>
+			);
+		}
+	};
 };
