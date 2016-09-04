65d764f2ad7a2562072f04891997853354b7e5b8
staskorz <staskorz@gmail.com>
2016-09-04 15:33:30 +0300

    Restyle take-exam dialog

--- imports/ui/pages/TakeExam.jsx
+++ imports/ui/pages/TakeExam.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import { FlatButton } from 'material-ui';
 
 import LoadingIndicator from '/imports/ui/components/LoadingIndicator';
 
 
 export default class TakeExam extends Component {
+	style = {
+		mainContainer: {
+			padding: '120px',
+			paddingTop: '20px'
+		},
+		
+		contentContainer: {
+			padding: '16px',
+			paddingTop: '0px'
+		}
+	};
+	
 	render() {
 		const { exam: { _id, name, number, numOfQuestions }, ready } = this.props;
 		
 		return (
-				<div>
+				<div style={ this.style.mainContainer }>
 					{ ready ?
 							<div>
 								<h1>Exam ID: { _id }</h1>
-								<p>Exam Name: { name }</p>
-								<p>Exam Number: { number }</p>
-								<p>Number of Questions: { numOfQuestions }</p>
+								<div style={ this.style.contentContainer }>
+									<p>Exam Name: { name }</p>
+									<p>Exam Number: { number }</p>
+									<p>Number of Questions: { numOfQuestions }</p>
+								</div>
 							</div>
 							
 							:
