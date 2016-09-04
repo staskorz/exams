fd9ce6ffcdf7ae67b85976fc87316aeffa9caa54
staskorz <staskorz@gmail.com>
2016-09-04 16:28:41 +0300

    Rearrange exam confirmation dialog

--- imports/ui/pages/TakeExam.jsx
+++ imports/ui/pages/TakeExam.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatButton } from 'material-ui';
+import { Card, CardTitle, CardText, CardActions, RaisedButton } from 'material-ui';
 
 import LoadingIndicator from '/imports/ui/components/LoadingIndicator';
 
@@ -11,11 +11,19 @@ export default class TakeExam extends Component {
 			paddingTop: '20px'
 		},
 		
-		contentContainer: {
-			padding: '16px',
-			paddingTop: '0px'
+		confirmationText: {
+			fontSize: '20px',
+			fontWeight: 'normal',
+			lineHeight: '36px',
+			fontFamily: 'Roboto, sans-serif',
+			color: 'rgba(0, 0, 0, 0.870588)'
+		},
+		
+		cardActions: {
+			padding: '16px'
 		}
 	};
+	
 	
 	render() {
 		const { exam: { _id, name, number, numOfQuestions }, ready } = this.props;
@@ -23,14 +31,21 @@ export default class TakeExam extends Component {
 		return (
 				<div style={ this.style.mainContainer }>
 					{ ready ?
-							<div>
-								<h1>Exam ID: { _id }</h1>
-								<div style={ this.style.contentContainer }>
-									<p>Exam Name: { name }</p>
-									<p>Exam Number: { number }</p>
-									<p>Number of Questions: { numOfQuestions }</p>
-								</div>
-							</div>
+							<Card>
+								<CardTitle
+										title={ 'Exam Name: ' + name }
+										subtitle={ 'Exam Number: ' + number + ', Number of Questions: ' + numOfQuestions }
+								/>
+								
+								<CardText>
+									<span style={ this.style.confirmationText }>Are you sure you want to take this exam <strong>now</strong>?</span>
+								</CardText>
+								
+								<CardActions style={ this.style.cardActions }>
+									<RaisedButton label='Yes' primary={ true } />
+									<RaisedButton label='No' />
+								</CardActions>
+							</Card>
 							
 							:
 							
