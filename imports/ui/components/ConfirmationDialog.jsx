c3960242605a3627027cb25f5ba193c8d459b324
staskorz <staskorz@gmail.com>
2016-09-08 15:42:42 +0300

    Remove unused import

--- imports/ui/components/ConfirmationDialog.jsx
+++ imports/ui/components/ConfirmationDialog.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { FloatingActionButton, Dialog, FlatButton } from 'material-ui';
+import { Dialog, FlatButton } from 'material-ui';
 
 
 export default class ConfirmedFloatingActionButton extends Component {
