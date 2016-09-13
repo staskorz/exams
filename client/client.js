eb05004c4a07e69346485fe997be8f0945a1d7cf
staskorz <staskorz@gmail.com>
2016-09-13 14:48:32 +0300

    Use absolute module paths

--- client/client.js
+++ client/client.js
@@ -1,4 +1,4 @@
-import '../imports/startup/client/simple-schema-messages-translation';
-import '../imports/startup/client/material-ui-touch-workaround';
-import '../imports/startup/client/styles';
-import '../imports/startup/client/react-ui';
+import '/imports/startup/client/simple-schema-messages-translation';
+import '/imports/startup/client/material-ui-touch-workaround';
+import '/imports/startup/client/styles';
+import '/imports/startup/client/react-ui';
