58c6f02358ef6b7cbd45a7167a3d7d6f93d27d17
staskorz <staskorz@gmail.com>
2016-09-13 17:28:12 +0300

    Add a method to shuffle questions and un-shuffle answers

---
+++ imports/client/shuffle-questions.js
@@ -0,0 +1,23 @@
+import arrayShuffle from 'lodash.shuffle';
+
+
+const shuffleArray = sourceArray => {
+	const arrayWithEmptyElements = new Array(sourceArray.length);
+	
+	const arrayWithOrderedNumbers = arrayWithEmptyElements.map((elem, index) => index);
+	
+	const arrayWithShuffledNumbers = arrayShuffle(arrayWithOrderedNumbers);
+	
+	const shuffledArray = arrayWithShuffledNumbers.map(number => sourceArray[number]);
+	
+	const unShuffle = (sourceArrayShuffled) => {
+		const targetArray = new Array(sourceArrayShuffled.length);
+
+		return targetArray.map((elem, index) => sourceArrayShuffled[arrayWithShuffledNumbers.indexOf(index)]);
+	};
+	
+	return {
+		shuffledArray,
+		unShuffle
+	};
+};
