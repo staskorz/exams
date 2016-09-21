49b3f827290672876e6aa89cafdd936cc1271c9d
staskorz <staskorz@gmail.com>
2016-09-21 14:30:33 +0300

    Create user account record upon successful login

--- imports/startup/server/accounts-sspi.js
+++ imports/startup/server/accounts-sspi.js
@@ -12,28 +12,22 @@ Meteor.methods({
 
 // Inspired by https://meteorhacks.com/extending-meteor-accounts/
 Accounts.registerLoginHandler('sspi', ({ sspi }) => {
-	if(sspi) {
-		console.log('sspi login request');
-	} else {
-		console.log('non-sspi login request');
-		
+	if(!sspi) {
 		return undefined;
 	}
 	
-	const sspiUser = Meteor.call('getSspiUser');
+	const username = Meteor.call('getSspiUser');
 	
-	console.log('sspiUser:', sspiUser);
+	console.log('username:', username);
 	
-	return null;
-	
-	const user = Meteor.users.findOne({ username: 'user001' });
+	const user = Meteor.users.findOne({ username });
 	
 	let userId;
 	
 	if(user) {
 		userId = user._id;
 	} else {
-		userId = Meteor.users.insert({ username: 'user001' });
+		userId = Meteor.users.insert({ username });
 	}
 	
 	console.log('userId:', userId);
