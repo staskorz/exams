3a9ddda45ab927c8072b94a2356e2abeb4ae5e86
staskorz <staskorz@gmail.com>
2016-09-21 11:27:53 +0300

    Remove commented-out code

--- imports/startup/server/sspi.js
+++ imports/startup/server/sspi.js
@@ -9,24 +9,6 @@ const app = express();
 const nodeSSPI = new NodeSSPI();
 
 
-// app.use((req, res, next) => {
-// 	console.log('sending headers');
-//	
-// 	res.set({
-// 		'Cache-Control': 'no-cache, no-store, must-revalidate',
-// 		'Pragma': 'no-cache',
-// 		'Expires': '0',
-// 		'Access-Control-Allow-Origin': 'http://localhost:3000',
-// 		// 'Access-Control-Allow-Headers': 'Authorization',
-// 		// 'Access-Control-Allow-Methods': 'GET, OPTIONS',
-// 		// 'Access-Control-Allow-Origin': '*',
-// 		// 'Access-Control-Expose-Headers': 'WWW-Authenticate'
-// 	});
-//	
-// 	next();
-// });
-//
-
 app.use((req, res, next) => {
 	nodeSSPI.authenticate(req, res, err => {
 		if(err) {
@@ -36,44 +18,6 @@ app.use((req, res, next) => {
 		res.finished || next();
 	});
 });
-
-
-// const upsertUser = async username => {
-// 	const user = await Meteor.users.findOne({ username });
-//	
-// 	let userId;
-//	
-// 	if(user) {
-// 		console.log('found user:', user);
-//		
-// 		userId = user._id;
-// 	} else {
-// 		userId = await Meteor.users.insert({ username });
-// 	}
-//	
-// 	return userId;
-// };
-//
-//
-// app.use((req, res) => {
-// 	const userWithDomain = req.connection.user;
-//	
-// 	console.log('Authenticated:', userWithDomain);
-//	
-// 	const username = userWithDomain.split('\\')[1];
-//	
-// 	res.send(username);
-//	
-// 	upsertUser(username).then(userId => {
-// 		console.log('username:', username, 'userId:', userId);
-//		
-// 		res.end();
-// 	}).catch(err => {
-// 		console.log('err:', err);
-//		
-// 		res.end();
-// 	});
-// });
 
 
 const meteorProxy = httpProxyMiddleware({
