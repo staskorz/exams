d1630d2ff70d03df5f5f4d7f9efed72a207ccbeb
staskorz <staskorz@gmail.com>
2016-09-25 13:25:47 +0300

    Add "answers" collection

---
+++ imports/api/answers/schema.js
@@ -0,0 +1,48 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+
+const question = new SimpleSchema({
+	answers: {
+		type: [Boolean],
+		minCount: 2,
+		maxCount: 4
+	}
+});
+
+
+export default new SimpleSchema({
+	_id: {
+		type: SimpleSchema.RegEx.Id,
+		optional: true,
+		autoValue: function() {
+			if(!this.isUpdate) {
+				this.unset();
+			}
+		}
+	},
+	
+	examId: {
+		type: SimpleSchema.RegEx.Id
+	},
+	
+	examTimestamp: {
+		type: Date,
+		optional: true,
+		autoValue: function() {
+			return new Date();
+		}
+	},
+	
+	examineeUserId: {
+		type: SimpleSchema.RegEx.Id,
+		optional: true,
+		autoValue: function() {
+			return this.userId;
+		}
+	},
+	
+	questions: {
+		type: [question],
+		minCount: 1
+	}
+});
