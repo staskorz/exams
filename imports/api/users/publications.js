6a6a0eb4ee0e5347c00ce39ae031f4758a3116a6
staskorz <staskorz@gmail.com>
2016-09-25 12:10:58 +0300

    Prevent users from updating their user records in DB

--- imports/api/users/publications.js
+++ imports/api/users/publications.js
@@ -1,6 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 
 
+Meteor.users.deny({
+	update: function() {
+		return true;
+	}
+});
+
+
 Meteor.publish('userData', function() {
 	if(this.userId) {
 		return Meteor.users.find({ _id: this.userId }, { fields: { 'role': 1 } });
