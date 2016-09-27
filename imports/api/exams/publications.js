11d6b602738782afe6eccce6f16f552111aebe69
staskorz <staskorz@gmail.com>
2016-09-27 11:55:13 +0300

    Make exams publication only available for operators

--- imports/api/exams/publications.js
+++ imports/api/exams/publications.js
@@ -1,22 +1,29 @@
 import { Meteor } from 'meteor/meteor';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
+import getUserRole from '/imports/api/utils/get-user-role';
 import collection from './collection';
 
 
-Meteor.publish('exams', () => collection.find(
-		{}, {
-			fields: {
-				_id: 1,
-				name: 1,
-				published: 1,
-				createdAt: 1,
-				updatedAt: 1,
-				questions: 1
-			},
+Meteor.publish('exams', function() {
+			if(getUserRole(this.userId) !== 'operator') {
+				throw new Meteor.Error('exams.notOperator', 'Available only for operators.');
+			}
 			
-			sort: { name: 1 }
-		})
+			return collection.find(
+					{}, {
+						fields: {
+							_id: 1,
+							name: 1,
+							published: 1,
+							createdAt: 1,
+							updatedAt: 1,
+							questions: 1
+						},
+						
+						sort: { name: 1 }
+					})
+		}
 );
 
 
