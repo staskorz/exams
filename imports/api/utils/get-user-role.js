d5ac8a5aae2a0b1be958d08ead1154da0a95c15b
staskorz <staskorz@gmail.com>
2016-09-27 11:53:00 +0300

    Return null if userId is null

--- imports/api/utils/get-user-role.js
+++ imports/api/utils/get-user-role.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 
 export default userId => {
+	if(!userId) {
+		return null;
+	}
+	
 	const user = Meteor.users.findOne({ _id: userId }, { fields: { 'role': 1 } });
 	
 	if(user) {
