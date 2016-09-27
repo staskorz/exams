364840c316ac4a026350a60a9a74a0d524f43c38
staskorz <staskorz@gmail.com>
2016-09-27 11:56:37 +0300

    Reformat code

--- imports/api/exams/publications.js
+++ imports/api/exams/publications.js
@@ -6,25 +6,23 @@ import collection from './collection';
 
 
 Meteor.publish('exams', function() {
-			if(getUserRole(this.userId) !== 'operator') {
-				throw new Meteor.Error('exams.notOperator', 'Available only for operators.');
-			}
-			
-			return collection.find(
-					{}, {
-						fields: {
-							_id: 1,
-							name: 1,
-							published: 1,
-							createdAt: 1,
-							updatedAt: 1,
-							questions: 1
-						},
-						
-						sort: { name: 1 }
-					})
-		}
-);
+	if(getUserRole(this.userId) !== 'operator') {
+		throw new Meteor.Error('exams.notOperator', 'Available only for operators.');
+	}
+	
+	return collection.find({}, {
+		fields: {
+			_id: 1,
+			name: 1,
+			published: 1,
+			createdAt: 1,
+			updatedAt: 1,
+			questions: 1
+		},
+		
+		sort: { name: 1 }
+	})
+});
 
 
 Meteor.publish('exams.published', () => collection.find(
