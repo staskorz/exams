daaac710d5786547185b8037a59a656fcc1d6027
staskorz <staskorz@gmail.com>
2016-09-27 17:36:05 +0300

    Display loading indicator while results are loading

--- imports/client/ui/pages/ExamResults.jsx
+++ imports/client/ui/pages/ExamResults.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import LoadingIndicator from '/imports/client/ui/components/LoadingIndicator';
+
 
 export default class ExamResults extends Component {
 	render() {
+		const { ready } = this.props;
+		
+		if(!ready) {
+			return <LoadingIndicator />;
+		}
+		
 		return (
 				<h1>Exam Results Placeholder</h1>
 		);
 	}
 };
-
