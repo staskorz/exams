85b8aa78ee4a6c47c5f5acbc09a291667ff4030f
staskorz <staskorz@gmail.com>
2016-09-28 13:19:17 +0300

    Convert exam results to .csv before exporting

--- imports/client/csv-export.js
+++ imports/client/csv-export.js
@@ -1,8 +1,9 @@
+import { unparse as jsonToCsv } from 'papaparse';
 import { saveAs } from 'file-saver';
 
 
 export default (filename, contentObj) => {
-	const blobContent = JSON.stringify(contentObj);
+	const blobContent = jsonToCsv(contentObj);
 	
 	const blob = new Blob([blobContent], { type: 'text/csv' });
 	saveAs(blob, filename);
