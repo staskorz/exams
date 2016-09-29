c2faf3db3a0309e59fdbfd84c51c0907e5b25b97
staskorz <staskorz@gmail.com>
2016-09-29 09:15:37 +0300

    Retrieve from AD and save to DB additional user properties

--- imports/startup/server/accounts-sspi.js
+++ imports/startup/server/accounts-sspi.js
@@ -19,6 +19,16 @@ Meteor.methods({
 		let isUserMemberOfAsyncToSync = Meteor.wrapAsync(activeDirectory.isUserMemberOf, activeDirectory);
 		
 		return isUserMemberOfAsyncToSync(samAccountName, group);
+	},
+	
+	getUserProperties({ samAccountName }) {
+		let findUserAsyncToSync = Meteor.wrapAsync(activeDirectory.findUser, activeDirectory);
+		
+		const opts = {
+			attributes: ['displayName', 'description', 'postalCode']
+		};
+		
+		return findUserAsyncToSync(opts, samAccountName);
 	}
 });
 
@@ -49,18 +59,32 @@ Accounts.registerLoginHandler('sspi', ({ sspi }) => {
 	
 	const role = isMember ? 'operator' : 'user';
 	
-	const user = Meteor.users.findOne({ username }, { fields: { role: 1 } });
+	const adUserProperties = Meteor.call('getUserProperties', { samAccountName });
+	
+	if(!adUserProperties) {
+		return {
+			type: 'sspi',
+			error: new Meteor.Error('sspi-could-not-retrieve-user-properties', 'Could not retrieve logged-in user properties')
+		};
+	}
+	
+	const { displayName: englishName, description: hebrewName, postalCode: employeeId } = adUserProperties;
+	
+	const user = Meteor.users.findOne({ username }, { fields: { role: 1, englishName: 1, hebrewName: 1, employeeId: 1 } });
 	
 	let userId;
 	
 	if(user) {
 		userId = user._id;
 		
-		if(role !== user.role) {
-			Meteor.users.update({ _id: userId }, { $set: { role } });
+		if(role !== user.role ||
+				((englishName || user.englishName) && (englishName !== user.englishName)) ||
+				((hebrewName || user.hebrewName) && (hebrewName !== user.hebrewName)) ||
+				((employeeId || user.employeeId) && (employeeId !== user.employeeId))) {
+			Meteor.users.update({ _id: userId }, { $set: { role, englishName, hebrewName, employeeId } });
 		}
 	} else {
-		userId = Meteor.users.insert({ username, role });
+		userId = Meteor.users.insert({ username, role, englishName, hebrewName, employeeId });
 	}
 	
 	return {
