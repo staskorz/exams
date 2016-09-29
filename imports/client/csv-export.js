7695be94de01b4c693a57e22b7323ce3b9edaa5f
staskorz <staskorz@gmail.com>
2016-09-29 10:40:07 +0300

    Export the csv in UTF-8

--- imports/client/csv-export.js
+++ imports/client/csv-export.js
@@ -5,6 +5,6 @@ import { saveAs } from 'file-saver';
 export default (filename, contentObj) => {
 	const blobContent = jsonToCsv(contentObj);
 	
-	const blob = new Blob([blobContent], { type: 'text/csv' });
+	const blob = new Blob([blobContent], { type: 'text/csv; charset=UTF-8' });
 	saveAs(blob, filename);
 };
