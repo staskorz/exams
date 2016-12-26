8c85e6d9c0967966c24429959909b18fbd552cc7
staskorz <staskorz@gmail.com>
2016-12-26 15:20:54 +0200

    Add modules to convert between blob and uint8array

---
+++ imports/client/blob-to-uint8array.js
@@ -0,0 +1,13 @@
+export default (blob, cb) => {
+	const fileReader = new FileReader();
+	
+	fileReader.onerror = () => {
+		cb('Error reading BLOB');
+	};
+	
+	fileReader.onload = function() {
+		cb(null, new Uint8Array(this.result));
+	};
+	
+	fileReader.readAsArrayBuffer(blob);
+}
---
+++ imports/client/uint8array-to-blob.js
@@ -0,0 +1,1 @@
+export default uint8arr => new Blob([uint8arr.buffer]);
