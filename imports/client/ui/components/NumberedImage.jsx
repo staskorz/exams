43f370f9b6e83f58c47f80fa6a2f49d4aaf055b2
staskorz <staskorz@gmail.com>
2016-12-27 14:01:46 +0200

    Add components for displaying the images

---
+++ imports/client/ui/components/NumberedImage.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { cyan500 } from 'material-ui/styles/colors';
+
+import uint8arrayToBlob from '/imports/client/uint8array-to-blob';
+import NumberBadge from './NumberBadge';
+
+
+export default class NumberedImage extends Component {
+	state = {
+		image: null
+	};
+	
+	
+	style = {
+		imageContainer: {
+			display: 'inline-block',
+			verticalAlign: 'top',
+			margin: '16px',
+			borderWidth: '2px',
+			borderColor: cyan500,
+			borderStyle: 'solid',
+			borderRadius: '4px',
+			padding: '3px'
+		},
+		
+		numberBadge: {
+			position: 'absolute',
+			padding: '12px 0px 12px 7px',
+			marginTop: '-20px'
+		}
+	};
+	
+	
+	cleanupImageState = () => {
+		const { image } = this.state;
+		
+		if(image) {
+			URL.revokeObjectURL(image);
+		}
+	};
+	
+	
+	setImageState = ({ image }) => {
+		this.cleanupImageState();
+		
+		if(image) {
+			this.setState({
+				image: URL.createObjectURL(uint8arrayToBlob(image))
+			});
+		} else {
+			this.setState({
+				image: null
+			});
+		}
+	};
+	
+	
+	componentWillMount() {
+		this.setImageState(this.props);
+	};
+	
+	
+	componentWillReceiveProps(nextProps) {
+		this.setImageState(nextProps);
+	};
+	
+	
+	componentWillUnmount() {
+		this.cleanupImageState();
+	};
+	
+	
+	render() {
+		const { image } = this.state;
+		const { number } = this.props;
+		
+		return <div style={ this.style.imageContainer }>
+			<NumberBadge content={ number } primary={ true } style={ this.style.numberBadge } />
+			
+			<img src={ image } alt={ 'image' + number } />
+		</div>;
+	};
+}
---
+++ imports/client/ui/components/NumberedImagesBlock.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NumberedImage from './NumberedImage';
+
+
+export default ({ images, style: propsStyle }) => {
+	if(!images || !images.length || !images[0]) {
+		return <div></div>;
+	}
+	
+	return <div style={ propsStyle }>
+		{ images.map((imageData, index) => imageData ?
+				<NumberedImage image={ imageData.image } number={ index + 1 } key={ index } /> : '')  }
+	</div>;
+}
