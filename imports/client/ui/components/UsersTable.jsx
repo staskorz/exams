7c509b2b8071888e4159df88a8eef9cbabc5eec0
staskorz <staskorz@gmail.com>
2016-12-29 15:07:06 +0200

    Use different style for operators

--- imports/client/ui/components/UsersTable.jsx
+++ imports/client/ui/components/UsersTable.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { Table, TableHeader, TableRow, TableHeaderColumn, TableBody, TableRowColumn, IconButton } from 'material-ui';
 import DeleteIcon from 'material-ui/svg-icons/action/delete';
 import ListIcon from 'material-ui/svg-icons/action/view-headline';
+import { pink50 } from 'material-ui/styles/colors';
 import { FormattedMessage } from 'react-intl';
 
 import LoadingIndicator from '/imports/client/ui/components/LoadingIndicator';
 
 
 const showCheckboxes = false;
+
+
+style = {
+	operator: {
+		background: pink50,
+		fontWeight: 'bold'
+	}
+};
 
 
 export default ({ ready, users }) => {
@@ -25,8 +34,8 @@ export default ({ ready, users }) => {
 			</TableRow>
 		</TableHeader>
 		<TableBody displayRowCheckbox={ showCheckboxes }>
-			{ users.map(({ hebrewName, employeeId, username }, index) => (
-					<TableRow key={ index }>
+			{ users.map(({ hebrewName, employeeId, username, role }, index) => (
+					<TableRow key={ index } style={ role === 'operator' ? style.operator : null }>
 						<TableRowColumn>{ hebrewName }</TableRowColumn>
 						<TableRowColumn>{ employeeId }</TableRowColumn>
 						<TableRowColumn>{ username }</TableRowColumn>
