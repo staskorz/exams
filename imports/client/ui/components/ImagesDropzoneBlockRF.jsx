18d1fadde716dfb110263c3021a52bc9c2b1a832
staskorz <staskorz@gmail.com>
2017-01-03 15:26:48 +0200

    Ensure value is being immediately updated in state

--- imports/client/ui/components/ImagesDropzoneBlockRF.jsx
+++ imports/client/ui/components/ImagesDropzoneBlockRF.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 import ImagesDropzoneBlock from './ImagesDropzoneBlock';
 
 
-export default ({ input: { value, onChange } }) => {
-	return <ImagesDropzoneBlock images={ value } onChange={ onChange } />;
+export default ({ input: { value, onChange, onBlur } }) => {
+	const onChangeLocal = val => {
+		onChange(val);
+		onBlur(val)
+	};
+	
+	return <ImagesDropzoneBlock images={ value } onChange={ onChangeLocal } />;
 }
