1023f3ac2fd82fadc379b9c8ecf3a5b387bb712b
staskorz <staskorz@gmail.com>
2017-03-21 13:58:04 +0200

    Global color constants

---
+++ imports/client/ui/colors.js
@@ -0,0 +1,6 @@
+import { cyan500 } from 'material-ui/styles/colors'
+
+export const primary = 'rgba(0, 0, 0, 0.870588)'
+export const neutral = cyan500
+export const positive = '#01bc01'
+export const attention = 'red'
