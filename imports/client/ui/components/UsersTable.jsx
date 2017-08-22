3470d5625f6afa8aafbb60e61e1681042280a5a4
staskorz <staskorz@gmail.com>
2017-08-22 17:24:50 +0300

    Use react-virtualized to boost table rendering performance

--- imports/client/ui/components/UsersTable.jsx
+++ imports/client/ui/components/UsersTable.jsx
@@ -1,52 +1,73 @@
-import React from 'react';
-import { Table, TableHeader, TableRow, TableHeaderColumn, TableBody, TableRowColumn, IconButton } from 'material-ui';
-import ListIcon from 'material-ui/svg-icons/editor/format-list-bulleted';
-import { pink50 } from 'material-ui/styles/colors';
-import { FormattedMessage } from 'react-intl';
-import { withRouter } from 'react-router';
+import React from 'react'
+import { WindowScroller, AutoSizer, Table, Column } from 'react-virtualized'
+import { neutral } from '/imports/client/ui/colors'
+import { injectIntl } from 'react-intl'
+import { withRouter, Link } from 'react-router'
 
-import LoadingIndicator from '/imports/client/ui/components/LoadingIndicator';
-
-
-const showCheckboxes = false;
+import LoadingIndicator from '/imports/client/ui/components/LoadingIndicator'
 
 
 const style = {
-	operator: {
-		background: pink50,
-		fontWeight: 'bold'
+	actionLink: {
+		color: neutral,
 	}
-};
+}
 
 
-const UsersTable = ({ loading, users, router }) => {
+const UsersTable = ({ loading, users, router, intl: { formatMessage } }) => {
 	if(loading) {
-		return <LoadingIndicator />;
+		return <LoadingIndicator />
 	}
 	
-	return <Table selectable={ false }>
-		<TableHeader displaySelectAll={ showCheckboxes } adjustForCheckbox={ showCheckboxes }>
-			<TableRow>
-				<TableHeaderColumn><FormattedMessage id='name' /></TableHeaderColumn>
-				<TableHeaderColumn><FormattedMessage id='employeeId' /></TableHeaderColumn>
-				<TableHeaderColumn><FormattedMessage id='username' /></TableHeaderColumn>
-				<TableHeaderColumn><FormattedMessage id='actions' /></TableHeaderColumn>
-			</TableRow>
-		</TableHeader>
-		<TableBody displayRowCheckbox={ showCheckboxes }>
-			{ users.map(({ _id, hebrewName, employeeId, username, role }, index) => (
-					<TableRow key={ index } style={ role === 'operator' ? style.operator : null }>
-						<TableRowColumn>{ hebrewName }</TableRowColumn>
-						<TableRowColumn>{ employeeId }</TableRowColumn>
-						<TableRowColumn>{ username }</TableRowColumn>
-						<TableRowColumn>
-							<IconButton onClick={ () => router.push('/user-results/' + _id) }><ListIcon /></IconButton>
-						</TableRowColumn>
-					</TableRow>
-			)) }
-		</TableBody>
-	</Table>;
-};
+	const translatedName = formatMessage({ id: 'name' })
+	const translatedEmployeeId = formatMessage({ id: 'employeeId' })
+	const translatedUsername = formatMessage({ id: 'username' })
+	const translatedMarks = formatMessage({ id: 'marks' })
+	const translatedActions = formatMessage({ id: 'actions' })
+	const translatedRoles = formatMessage({ id: 'roles' })
+	const translatedOperator = formatMessage({ id: 'operator' })
+	
+	const actionsCellRenderer = ({ rowData }) => <Link style={ style.actionLink } to={ '/user-results/' + rowData._id }>
+		{ translatedMarks }
+	</Link>
+	
+	const rolesCellRenderer = ({ rowData: { role } }) => role === 'operator' ? translatedOperator : null
+	
+	const numColumns = 5
+	
+	return <WindowScroller>
+		{({ height, isScrolling, scrollTop }) => <AutoSizer disableHeight>
+			{ ({ width }) => <Table
+					rowCount={ users.length }
+					rowGetter={ ({ index }) => users[index] }
+					rowHeight={ 48 }
+					headerHeight={ 58 }
+					width={ width }
+					height={ height }
+					scrollTop={ scrollTop }
+					autoHeight
+			>
+				<Column dataKey='hebrewName' label={ translatedName } width={ width / numColumns } />
+				<Column dataKey='employeeId' label={ translatedEmployeeId } width={ width / numColumns } />
+				<Column dataKey='username' label={ translatedUsername } width={ width / numColumns } />
+				
+				<Column
+						dataKey='roles'
+						label={ translatedRoles }
+						width={ width / numColumns }
+						cellRenderer={ rolesCellRenderer }
+				/>
+				
+				<Column
+						dataKey='actions'
+						label={ translatedActions }
+						width={ width / numColumns }
+						cellRenderer={ actionsCellRenderer }
+				/>
+			</Table> }
+		</AutoSizer> }
+	</WindowScroller>
+}
 
 
-export default withRouter(UsersTable);
+export default injectIntl(withRouter(UsersTable))
--- imports/startup/client/styles.js
+++ imports/startup/client/styles.js
@@ -1,1 +1,3 @@
+import 'react-virtualized/styles.css';
+
 import '../../client/static/main.css';
