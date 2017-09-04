b7b5e107bc38797da40b537b1f895d0532f370f3
staskorz <staskorz@gmail.com>
2017-09-04 15:13:23 +0300

    Add title

--- imports/client/ui/pages/QuestionnaireCreate/index.js
+++ imports/client/ui/pages/QuestionnaireCreate/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl';
 
 import Form from './Form'
 
 
 export default () => <div>
+	<h1><FormattedMessage id='createQuestionnaire' /></h1>
+	
 	<Form />
 </div>
