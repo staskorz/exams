5b4620bb82a032127e468608879f9921fed597d2
staskorz <staskorz@gmail.com>
2017-09-12 16:24:42 +0300

    Make the label and the checkbox closer to one another

--- imports/client/ui/pages/QuestionnaireCreate/Form/Checkbox.jsx
+++ imports/client/ui/pages/QuestionnaireCreate/Form/Checkbox.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Checkbox } from 'material-ui'
 
+
+const style = {
+	iconStyle: {
+		marginRight: '6px',
+	},
+}
+
+
 export default ({ onChange, value, ...props }) => <Checkbox
 		{ ...props }
 		checked={ value }
 		onCheck={ (e, value) => onChange(value) }
+		iconStyle={ style.iconStyle }
 />
