4e9685a4802bb989fd55898e191edfd846458dbb
staskorz <staskorz@gmail.com>
2017-09-18 16:17:00 +0300

    Remove logging

--- imports/client/ui/pages/QuestionnaireCreate/with-create.js
+++ imports/client/ui/pages/QuestionnaireCreate/with-create.js
@@ -5,8 +5,6 @@ import { insert } from '/imports/api/questionnaires/methods'
 
 export default withHandlers({
 	onSave: ({ value, router }) => () => {
-		console.log('Saving...')
-		
 		insert.call(value, (error, result) => {
 			if(error) {
 				console.log('insertQuestionnaire error:', error)
