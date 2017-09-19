ecd270c9b99d9fc4bb1920711b8f986ef706f9ec
staskorz <staskorz@gmail.com>
2017-09-19 08:41:39 +0300

    Move state and handlers into a separate module

--- imports/client/ui/pages/QuestionnaireCreate/enhance.js
+++ imports/client/ui/pages/QuestionnaireCreate/enhance.js
@@ -1,115 +1,16 @@
-import { compose, withStateHandlers } from 'recompose'
+import { compose } from 'recompose'
 import { injectIntl } from 'react-intl'
 import { withRouter } from 'react-router'
 
-import validate from './validate'
 import withCancel from './with-cancel'
 import withCreate from './with-create'
-
-
-const initialFormValue = {
-	name: 'TestQuestionnaire',
-	published: true,
-	
-	questions: [
-		{
-			text: 'Question 1 Text',
-			
-			answers: [
-				{
-					text: 'Question 1 Answer 1 Text',
-				},
-				
-				{
-					text: 'Question 1 Answer 2 Text',
-					freeText: true,
-				},
-				
-				{
-					text: 'Question 1 Answer 3 Text',
-				},
-			],
-		},
-		
-		{
-			text: 'Question 2 Text',
-			multipleChoice: true,
-			
-			answers: [
-				{
-					text: 'Question 2 Answer 1 Text',
-				},
-				
-				{
-					text: 'Question 2 Answer 2 Text',
-				},
-				
-				{
-					text: 'Question 2 Answer 3 Text',
-					freeText: true,
-				},
-			],
-		},
-		
-		{
-			text: 'Question 3 Text',
-			multipleChoice: true,
-			
-			answers: [
-				{
-					text: 'Question 3 Answer 1 Text',
-				},
-				
-				{
-					text: 'Question 3 Answer 2 Text',
-				},
-				
-				{
-					text: 'Question 3 Answer 3 Text',
-					freeText: true,
-				},
-			],
-		},
-		
-		{
-			text: 'Question 4 Text',
-			multipleChoice: true,
-			
-			answers: [
-				{
-					text: 'Question 4 Answer 1 Text',
-				},
-				
-				{
-					text: 'Question 4 Answer 2 Text',
-				},
-				
-				{
-					text: 'Question 4 Answer 3 Text',
-					freeText: true,
-				},
-			],
-		},
-	],
-}
+import withStateAndStateHandlers from './with-state-and-state-handlers'
 
 
 export default compose(
 		withRouter,
 		withCancel,
 		injectIntl,
-		withStateHandlers(({ initialValue = initialFormValue, intl: { formatMessage } }) => ({
-			value: initialValue,
-			...validate(initialValue, formatMessage),
-		}), {
-			setValue: ({ value }, { intl: { formatMessage } }) => fn => {
-				const newValue = fn(value)
-				
-				return {
-					value: newValue,
-					...validate(newValue, formatMessage),
-				}
-			},
-		}),
+		withStateAndStateHandlers,
 		withCreate,
 )
---
+++ imports/client/ui/pages/QuestionnaireCreate/with-state-and-state-handlers.js
@@ -0,0 +1,105 @@
+import { withStateHandlers } from 'recompose'
+
+import validate from './validate'
+
+
+const initialFormValue = {
+	name: 'TestQuestionnaire',
+	published: true,
+	
+	questions: [
+		{
+			text: 'Question 1 Text',
+			
+			answers: [
+				{
+					text: 'Question 1 Answer 1 Text',
+				},
+				
+				{
+					text: 'Question 1 Answer 2 Text',
+					freeText: true,
+				},
+				
+				{
+					text: 'Question 1 Answer 3 Text',
+				},
+			],
+		},
+		
+		{
+			text: 'Question 2 Text',
+			multipleChoice: true,
+			
+			answers: [
+				{
+					text: 'Question 2 Answer 1 Text',
+				},
+				
+				{
+					text: 'Question 2 Answer 2 Text',
+				},
+				
+				{
+					text: 'Question 2 Answer 3 Text',
+					freeText: true,
+				},
+			],
+		},
+		
+		{
+			text: 'Question 3 Text',
+			multipleChoice: true,
+			
+			answers: [
+				{
+					text: 'Question 3 Answer 1 Text',
+				},
+				
+				{
+					text: 'Question 3 Answer 2 Text',
+				},
+				
+				{
+					text: 'Question 3 Answer 3 Text',
+					freeText: true,
+				},
+			],
+		},
+		
+		{
+			text: 'Question 4 Text',
+			multipleChoice: true,
+			
+			answers: [
+				{
+					text: 'Question 4 Answer 1 Text',
+				},
+				
+				{
+					text: 'Question 4 Answer 2 Text',
+				},
+				
+				{
+					text: 'Question 4 Answer 3 Text',
+					freeText: true,
+				},
+			],
+		},
+	],
+}
+
+
+export default withStateHandlers(({ initialValue = initialFormValue, intl: { formatMessage } }) => ({
+	value: initialValue,
+	...validate(initialValue, formatMessage),
+}), {
+	setValue: ({ value }, { intl: { formatMessage } }) => fn => {
+		const newValue = fn(value)
+		
+		return {
+			value: newValue,
+			...validate(newValue, formatMessage),
+		}
+	},
+})
