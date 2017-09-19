0f30373bc75e6f5015b7e6e63380b7f2e39e4d73
staskorz <staskorz@gmail.com>
2017-09-19 15:20:23 +0300

    Remove dummy data from new questionnaire

--- imports/client/ui/components/QuestionnaireEditForm/with-state-and-state-handlers.js
+++ imports/client/ui/components/QuestionnaireEditForm/with-state-and-state-handlers.js
@@ -4,85 +4,20 @@ import validate from './validate'
 
 
 const initialFormValue = {
-	name: 'TestQuestionnaire',
+	name: '',
 	published: true,
 	
 	questions: [
 		{
-			text: 'Question 1 Text',
+			text: '',
 			
 			answers: [
 				{
-					text: 'Question 1 Answer 1 Text',
+					text: '',
 				},
 				
 				{
-					text: 'Question 1 Answer 2 Text',
-					freeText: true,
-				},
-				
-				{
-					text: 'Question 1 Answer 3 Text',
-				},
-			],
-		},
-		
-		{
-			text: 'Question 2 Text',
-			multipleChoice: true,
-			
-			answers: [
-				{
-					text: 'Question 2 Answer 1 Text',
-				},
-				
-				{
-					text: 'Question 2 Answer 2 Text',
-				},
-				
-				{
-					text: 'Question 2 Answer 3 Text',
-					freeText: true,
-				},
-			],
-		},
-		
-		{
-			text: 'Question 3 Text',
-			multipleChoice: true,
-			
-			answers: [
-				{
-					text: 'Question 3 Answer 1 Text',
-				},
-				
-				{
-					text: 'Question 3 Answer 2 Text',
-				},
-				
-				{
-					text: 'Question 3 Answer 3 Text',
-					freeText: true,
-				},
-			],
-		},
-		
-		{
-			text: 'Question 4 Text',
-			multipleChoice: true,
-			
-			answers: [
-				{
-					text: 'Question 4 Answer 1 Text',
-				},
-				
-				{
-					text: 'Question 4 Answer 2 Text',
-				},
-				
-				{
-					text: 'Question 4 Answer 3 Text',
-					freeText: true,
+					text: '',
 				},
 			],
 		},
