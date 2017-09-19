7d519699e727a5d36bed443f51e5a3c9c99d4fb3
staskorz <staskorz@gmail.com>
2017-09-19 08:43:53 +0300

    Move form-related HOCs to Form component folder

--- imports/client/ui/pages/QuestionnaireCreate/enhance.js
+++ imports/client/ui/pages/QuestionnaireCreate/enhance.js
@@ -2,9 +2,9 @@ import { compose } from 'recompose'
 import { injectIntl } from 'react-intl'
 import { withRouter } from 'react-router'
 
-import withCancel from './with-cancel'
-import withCreate from './with-create'
-import withStateAndStateHandlers from './with-state-and-state-handlers'
+import withCancel from './Form/with-cancel'
+import withCreate from './Form/with-create'
+import withStateAndStateHandlers from './Form/with-state-and-state-handlers'
 
 
 export default compose(
