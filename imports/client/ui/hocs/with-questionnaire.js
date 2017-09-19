67670b7f9dc9c0af01992ff440231761f14a2372
staskorz <staskorz@gmail.com>
2017-09-19 17:30:00 +0300

    Make sure "loading" prop is initially set to true

--- imports/client/ui/hocs/with-questionnaire.js
+++ imports/client/ui/hocs/with-questionnaire.js
@@ -1,25 +1,27 @@
-import { lifecycle } from 'recompose'
+import { compose, withProps, lifecycle } from 'recompose'
 
 import { findOne } from '../../../api/questionnaires/methods'
 
 
-export default withQuestionnaire = lifecycle({
-	componentDidMount() {
-		this.setState({
+export default withQuestionnaire = compose(
+		withProps({
 			loading: true,
-		})
+		}),
 		
-		const { questionnaireId } = this.props.router.params
-		
-		findOne.call({ questionnaireId }, (err, questionnaire) => {
-			if(err) {
-				console.log('questionnaires.findOne error:', err)
-			} else {
-				this.setState({
-					loading: false,
-					initialValue: questionnaire,
+		lifecycle({
+			componentDidMount() {
+				const { questionnaireId } = this.props.router.params
+				
+				findOne.call({ questionnaireId }, (err, questionnaire) => {
+					if(err) {
+						console.log('questionnaires.findOne error:', err)
+					} else {
+						this.setState({
+							loading: false,
+							initialValue: questionnaire,
+						})
+					}
 				})
-			}
-		})
-	},
-})
+			},
+		}),
+)
