cd36d2753e196eb0c8136afe930ee5a475181c00
staskorz <staskorz@gmail.com>
2017-09-28 16:18:30 +0300

    Display description in questionnaire

--- imports/client/ui/pages/QuestionnaireTake/Form/index.js
+++ imports/client/ui/pages/QuestionnaireTake/Form/index.js
@@ -11,11 +11,19 @@ const style = {
 	title: {
 		margin: '0',
 	},
+	
+	description: {
+		marginRight: '17px',
+		marginBottom: '32px',
+		fontFamily: 'Roboto, sans-serif',
+	},
 }
 
 
-export default ({ questionnaire: { name, questions } }) => <form style={ style.form }>
+export default ({ questionnaire: { name, description, questions } }) => <form style={ style.form }>
 	<h1 style={ style.title }>{ name }</h1>
+	
+	<pre style={ style.description }>{ description }</pre>
 	
 	{ questions.map((question, index) => <Question
 			key={ index }
