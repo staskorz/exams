ac7a2a0dcb0ae5aa6127c51eec212ae6e27a04e1
staskorz <staskorz@gmail.com>
2017-10-19 17:25:30 +0300

    Reorder imports

--- imports/client/ui/pages/QuestionnaireAnswers/enhance.js
+++ imports/client/ui/pages/QuestionnaireAnswers/enhance.js
@@ -2,8 +2,9 @@ import { compose } from 'recompose'
 import { withRouter } from 'react-router'
 import { injectIntl } from 'react-intl'
 
-import withQuestionnaireAnswers from './with-questionnaire-answers'
 import withLoadingIndicator from '../../hocs/with-loading-indicator'
+
+import withQuestionnaireAnswers from './with-questionnaire-answers'
 
 
 export default compose(
