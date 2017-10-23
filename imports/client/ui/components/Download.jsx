e0d145872317c5794aae5746020b272e207a62a6
staskorz <staskorz@gmail.com>
2017-10-23 09:53:07 +0300

    Allow downloading questionnaire answers

---
+++ imports/client/ui/components/Download.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { IconButton } from 'material-ui'
+import DownloadIcon from 'material-ui/svg-icons/file/file-download'
+
+import csvExport from '../../csv-export'
+
+
+const handleClick = (generate, filename) => () => {
+	csvExport(filename, generate())
+}
+
+
+export default ({ generate, filename }) => <IconButton
+		onClick={ handleClick(generate, filename) }>
+	<DownloadIcon />
+</IconButton>
--- imports/client/ui/pages/QuestionnaireAnswers/index.js
+++ imports/client/ui/pages/QuestionnaireAnswers/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import dateJsToCsv from '../../../date-js-to-csv'
+import Download from '../../components/Download'
 
 import enhance from './enhance'
 import Table from './Table'
 
 
-const EnhancedTable = enhance(Table)
+const flattenQuestions = questions => questions.reduce((accQuestion, { answers }, questionIndex) => ({
+	...accQuestion,
+	...answers.reduce((accAnswer, { checked, freeText }, answerIndex) => ({
+		...accAnswer,
+		['q' + (questionIndex + 1) + 'a' + (answerIndex + 1) + 'c']: checked,
+		['q' + (questionIndex + 1) + 'a' + (answerIndex + 1) + 't']: freeText,
+	}), {})
+}), {})
 
 
-export default () => <EnhancedTable />
+const generate = questionnaireAnswers => () => questionnaireAnswers.map(({ timestamp, questions, userId, _id, questionnaireId, ...rest }) => ({
+	timestamp: dateJsToCsv(timestamp),
+	...rest,
+	...flattenQuestions(questions),
+}))
+
+
+export default enhance(props => <div>
+	<div>
+		<Download
+				generate={ generate(props.questionnaireAnswers) }
+				filename={ props.intl.formatMessage({ id: 'questionnaireAnswers' }) + '.csv' }
+		/>
+	</div>
+	
+	<Table { ...props } />
+</div>)
