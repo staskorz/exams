16fa7e18541e4092d339e0fdb9a0b86f996201cb
staskorz <staskorz@gmail.com>
2017-10-29 08:42:09 +0200

    Add filtering capability to questionnaires list

--- imports/client/ui/pages/QuestionnairesList/enhance.js
+++ imports/client/ui/pages/QuestionnairesList/enhance.js
@@ -4,10 +4,12 @@ import { injectIntl } from 'react-intl'
 import withLoadingIndicator from '../../hocs/with-loading-indicator'
 
 import withQuestionnaires from './with-questionnaires'
+import withFilter from './with-filter'
 
 
 export default compose(
 		withQuestionnaires,
 		withLoadingIndicator,
 		injectIntl,
+		withFilter,
 )
