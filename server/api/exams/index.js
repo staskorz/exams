cfce9d818afdd6c270f9f1e9afc34e3db3a12544
staskorz <staskorz@gmail.com>
2017-11-12 15:42:53 +0200

    Add a module to generate Meteor-like MongoDB ids

--- server/api/exams/index.js
+++ server/api/exams/index.js
@@ -28,4 +28,35 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/:examId', (req, res) => {
+	const { db } = req
+	
+	const examId = req.params['examId']
+	
+	const examsCollection = db.collection('Exams')
+	
+	const fields = {
+		name: 1,
+		published: 1,
+		questions: 1,
+	}
+	
+	examsCollection.findOne({ _id: examId }, fields).then(exam => {
+		if(!exam) {
+			res.status(404).send('Exam not found')
+			
+			return
+		}
+		
+		res.json(exam)
+	}).catch(err => {
+		const errorMessage = 'Error fetching exam from DB.'
+		
+		res.status(500).send(errorMessage)
+		
+		throw new Error(errorMessage + ' ' + err.name + ': ', err.message)
+	})
+})
+
+
 export default router
