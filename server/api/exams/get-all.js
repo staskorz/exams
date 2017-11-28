d73b2fd828be927e4e10b66f630c5a1b78343758
staskorz <staskorz@gmail.com>
2017-11-28 17:08:08 +0200

    Factor out rest endpoints into dedicated modules

---
+++ server/api/exams/get-all.js
@@ -0,0 +1,22 @@
+export default (req, res) => {
+	const { db } = req
+	
+	const examsCollection = db.collection('Exams')
+	
+	const fields = {
+		name: 1,
+		published: 1,
+		createdAt: 1,
+		updatedAt: 1,
+	}
+	
+	examsCollection.find({}, fields).toArray().then(exams => {
+		res.json(exams)
+	}).catch(err => {
+		const errorMessage = 'Error fetching exams from DB.'
+		
+		res.status(500).send(errorMessage)
+		
+		throw new Error(errorMessage + ' ' + err.name + ': ', err.message)
+	})
+}
--- server/api/exams/index.js
+++ server/api/exams/index.js
@@ -1,62 +1,15 @@
 import { Router } from 'express'
 
+import getAll from './get-all'
+import getById from './get-by-id'
 
 const router = Router()
 
 
-router.get('/', (req, res) => {
-	const { db } = req
-	
-	const examsCollection = db.collection('Exams')
-	
-	const fields = {
-		name: 1,
-		published: 1,
-		createdAt: 1,
-		updatedAt: 1,
-	}
-	
-	examsCollection.find({}, fields).toArray().then(exams => {
-		res.json(exams)
-	}).catch(err => {
-		const errorMessage = 'Error fetching exams from DB.'
-		
-		res.status(500).send(errorMessage)
-		
-		throw new Error(errorMessage + ' ' + err.name + ': ', err.message)
-	})
-})
+router.get('/', getAll)
 
 
-router.get('/:examId', (req, res) => {
-	const { db } = req
-	
-	const examId = req.params['examId']
-	
-	const examsCollection = db.collection('Exams')
-	
-	const fields = {
-		name: 1,
-		published: 1,
-		questions: 1,
-	}
-	
-	examsCollection.findOne({ _id: examId }, fields).then(exam => {
-		if(!exam) {
-			res.status(404).send('Exam not found')
-			
-			return
-		}
-		
-		res.json(exam)
-	}).catch(err => {
-		const errorMessage = 'Error fetching exam from DB.'
-		
-		res.status(500).send(errorMessage)
-		
-		throw new Error(errorMessage + ' ' + err.name + ': ', err.message)
-	})
-})
+router.get('/:examId', getById)
 
 
 export default router
