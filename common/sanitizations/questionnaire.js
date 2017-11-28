0f9aa96d70b740e3ee105e750c53f4c44a3d95c1
staskorz <staskorz@gmail.com>
2017-11-28 11:24:56 +0200

    Allow missing multipleChoice field

--- common/sanitizations/questionnaire.js
+++ common/sanitizations/questionnaire.js
@@ -2,8 +2,9 @@ export default ({ name, questions, description, published }) => ({
 	name,
 	description,
 	published,
-	questions: questions.map(({ text, answers }) => ({
+	questions: questions.map(({ text, multipleChoice, answers }) => ({
 		text,
+		multipleChoice,
 		answers: answers.map(({ freeText, text }) => ({
 			freeText,
 			text,
