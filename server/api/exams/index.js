bb00cfd40ed1298f507aea282b5565a8c49f326e
staskorz <staskorz@gmail.com>
2017-11-28 17:08:33 +0200

    Rearrange code

--- server/api/exams/index.js
+++ server/api/exams/index.js
@@ -3,12 +3,11 @@ import { Router } from 'express'
 import getAll from './get-all'
 import getById from './get-by-id'
 
+
 const router = Router()
 
 
 router.get('/', getAll)
-
-
 router.get('/:examId', getById)
 
 
