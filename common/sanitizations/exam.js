45432736425d75799e3d88ab6beb10132551c95d
staskorz <staskorz@gmail.com>
2017-11-29 17:01:03 +0200

    Implement sanitization for exams

--- common/sanitizations/exam.js
+++ common/sanitizations/exam.js
@@ -1,1 +1,30 @@
-export default exam => exam
+const sanitizeImage = image => {
+	if(!image) {
+		return
+	}
+	
+	const { height, width, imageBlob } = image
+	
+	return {
+		height,
+		width,
+		imageBlob: {
+			blob: imageBlob.blob,
+		},
+	}
+}
+
+
+export default ({ name, published, questions }) => ({
+	name,
+	published,
+	questions: questions.map(({ answers, images, text, weight }) => ({
+		text,
+		weight,
+		answers: answers.map(({ text, correct }) => ({
+			text,
+			correct,
+		})),
+		images: images.map(sanitizeImage),
+	})),
+})
