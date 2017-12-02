2685caf61a39f46f400c210ddaca552b1577c9b0
staskorz <staskorz@gmail.com>
2017-12-02 22:43:31 +0200

    Make component functional

--- imports/client/ui/pages/ExamTake/ExamInfo.jsx
+++ imports/client/ui/pages/ExamTake/ExamInfo.jsx
@@ -1,77 +1,66 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Paper, RaisedButton } from 'material-ui'
-import { withRouter } from 'react-router'
 import { FormattedMessage } from 'react-intl'
 
 import { canGoBack } from '../../browserHistoryCounter'
 
 
-class ExamInfo extends Component {
-	style = {
-		mainPaper: {
-			padding: '32px',
-		},
-		
-		descriptionText: {
-			fontSize: '16px',
-			fontWeight: 'normal',
-			lineHeight: '36px',
-			fontFamily: 'Roboto, sans-serif',
-			color: 'rgba(0, 0, 0, 0.870588)',
-			marginRight: '16px',
-			width: '200px',
-			display: 'inline-block',
-		},
-		
-		mainText: {
-			fontSize: '24px',
-			fontWeight: 'normal',
-			lineHeight: '36px',
-			fontFamily: 'Roboto, sans-serif',
-			color: 'rgba(0, 0, 0, 0.870588)',
-		},
-		
-		actions: {
-			paddingTop: '32px',
-		},
-		
-		button: {
-			marginRight: '16px',
-		},
-	}
+const style = {
+	mainPaper: {
+		padding: '32px',
+	},
 	
+	descriptionText: {
+		fontSize: '16px',
+		fontWeight: 'normal',
+		lineHeight: '36px',
+		fontFamily: 'Roboto, sans-serif',
+		color: 'rgba(0, 0, 0, 0.870588)',
+		marginRight: '16px',
+		width: '200px',
+		display: 'inline-block',
+	},
 	
-	render() {
-		const { exam, router, start } = this.props
-		const { name, questions } = exam || {}
-		
-		return <div className='main-container-padding'>
-			<Paper style={ this.style.mainPaper }>
-				<div>
-					<span style={ this.style.descriptionText }><FormattedMessage id='examName' />:</span>
-					<span style={ this.style.mainText }>{ name }</span>
-				</div>
-				
-				<div>
-					<span style={ this.style.descriptionText }><FormattedMessage id='numberOfQuestions' />:</span>
-					<span style={ this.style.mainText }>{ questions.length }</span>
-				</div>
-				
-				<div style={ this.style.actions }>
-					<RaisedButton label={ <FormattedMessage id='start' /> } primary={ true } style={ this.style.button }
-							onClick={ start } />
-					{ canGoBack() ?
-							<RaisedButton label={ <FormattedMessage id='cancel' /> } onClick={ () => router.goBack() } />
-							
-							:
-							
-							null
-					}
-				</div>
-			</Paper>
-		</div>
-	}
+	mainText: {
+		fontSize: '24px',
+		fontWeight: 'normal',
+		lineHeight: '36px',
+		fontFamily: 'Roboto, sans-serif',
+		color: 'rgba(0, 0, 0, 0.870588)',
+	},
+	
+	actions: {
+		paddingTop: '32px',
+	},
+	
+	button: {
+		marginRight: '16px',
+	},
 }
 
 
-export default withRouter(ExamInfo)
+export default ({ exam: { name, questions }, router, start }) => <div className='main-container-padding'>
+	<Paper style={ style.mainPaper }>
+		<div>
+			<span style={ style.descriptionText }><FormattedMessage id='examName' />:</span>
+			<span style={ style.mainText }>{ name }</span>
+		</div>
+		
+		<div>
+			<span style={ style.descriptionText }><FormattedMessage id='numberOfQuestions' />:</span>
+			<span style={ style.mainText }>{ questions.length }</span>
+		</div>
+		
+		<div style={ style.actions }>
+			<RaisedButton label={ <FormattedMessage id='start' /> } primary={ true } style={ style.button }
+					onClick={ start } />
+			{ canGoBack() ?
+					<RaisedButton label={ <FormattedMessage id='cancel' /> } onClick={ () => router.goBack() } />
+					
+					:
+					
+					null
+			}
+		</div>
+	</Paper>
+</div>
