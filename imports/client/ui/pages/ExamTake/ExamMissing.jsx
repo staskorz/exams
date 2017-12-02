1e344e47b220bbca59309032696c21015ab15acb
staskorz <staskorz@gmail.com>
2017-12-02 22:44:51 +0200

    Make component functional

--- imports/client/ui/pages/ExamTake/ExamMissing.jsx
+++ imports/client/ui/pages/ExamTake/ExamMissing.jsx
@@ -1,45 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Paper, RaisedButton } from 'material-ui'
-import { withRouter } from 'react-router'
 import { FormattedMessage } from 'react-intl'
 
 
-class ExamInfo extends Component {
-	style = {
-		mainPaper: {
-			padding: '32px',
-		},
-		
-		mainText: {
-			fontSize: '24px',
-			fontWeight: 'normal',
-			lineHeight: '36px',
-			fontFamily: 'Roboto, sans-serif',
-			color: 'rgba(0, 0, 0, 0.870588)',
-		},
-		
-		actions: {
-			paddingTop: '32px',
-		},
-	}
+const style = {
+	mainPaper: {
+		padding: '32px',
+	},
 	
+	mainText: {
+		fontSize: '24px',
+		fontWeight: 'normal',
+		lineHeight: '36px',
+		fontFamily: 'Roboto, sans-serif',
+		color: 'rgba(0, 0, 0, 0.870588)',
+	},
 	
-	render() {
-		const { router } = this.props
-		
-		return <div className='main-container-padding'>
-			<Paper style={ this.style.mainPaper }>
-				<div>
-					<span style={ this.style.mainText }><FormattedMessage id='examMissing' /></span>
-				</div>
-				
-				<div style={ this.style.actions }>
-					<RaisedButton label={ <FormattedMessage id='exit' /> } primary={ true } onClick={ () => router.goBack() } />
-				</div>
-			</Paper>
-		</div>
-	}
+	actions: {
+		paddingTop: '32px',
+	},
 }
 
 
-export default withRouter(ExamInfo)
+export default ({ router }) => <div className='main-container-padding'>
+	<Paper style={ this.style.mainPaper }>
+		<div>
+			<span style={ this.style.mainText }><FormattedMessage id='examMissing' /></span>
+		</div>
+		
+		<div style={ this.style.actions }>
+			<RaisedButton label={ <FormattedMessage id='exit' /> } primary={ true } onClick={ () => router.goBack() } />
+		</div>
+	</Paper>
+</div>
