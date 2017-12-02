00b3fbfb8fa17e7bd72fc972fd46f47b94fd8dff
staskorz <staskorz@gmail.com>
2017-12-02 22:45:57 +0200

    Fix accessing style constant

--- imports/client/ui/pages/ExamTake/ExamMissing.jsx
+++ imports/client/ui/pages/ExamTake/ExamMissing.jsx
@@ -23,12 +23,12 @@ const style = {
 
 
 export default ({ router }) => <div className='main-container-padding'>
-	<Paper style={ this.style.mainPaper }>
+	<Paper style={ style.mainPaper }>
 		<div>
-			<span style={ this.style.mainText }><FormattedMessage id='examMissing' /></span>
+			<span style={ style.mainText }><FormattedMessage id='examMissing' /></span>
 		</div>
 		
-		<div style={ this.style.actions }>
+		<div style={ style.actions }>
 			<RaisedButton label={ <FormattedMessage id='exit' /> } primary={ true } onClick={ () => router.goBack() } />
 		</div>
 	</Paper>
