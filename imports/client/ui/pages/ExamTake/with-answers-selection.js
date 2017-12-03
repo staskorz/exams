f4b7c74d09d14db00b72229176ba69e39eb97d58
staskorz <staskorz@gmail.com>
2017-12-03 22:35:05 +0200

    Handle both single (radio) and multiple selections

--- imports/client/ui/pages/ExamTake/with-answers-selection.js
+++ imports/client/ui/pages/ExamTake/with-answers-selection.js
@@ -3,17 +3,31 @@ import { withStateHandlers } from 'recompose'
 import replaceArrayElement from '../../../replace-array-element'
 
 
+const handleMultiSelection = replaceArrayElement
+
+
+const handleRadioSelection = (answers, answerNumber, checked) => {
+	if(checked) {
+		return answers.map((_, index) => answerNumber === index)
+	} else {
+		return handleMultiSelection(answers, answerNumber, false)
+	}
+}
+
+
 export default withStateHandlers(
 		({ exam: { questions } }) => ({
 			answers: questions.map(({ answers }) => new Array(answers.length).fill(false)),
 		}),
 		
 		{
-			answerSelectionHandler: ({ answers }, { questionNumber, exam }) => (e, isChecked) => {
+			answerSelectionHandler: ({ answers }, { questionNumber, exam: { questions } }) => (e, isChecked) => {
 				const answerNumber = parseInt(e.target.name)
 				
+				const handleSelection = questions[questionNumber].multiple ? handleMultiSelection : handleRadioSelection
+				
 				return {
-					answers: replaceArrayElement(answers, questionNumber, replaceArrayElement(answers[questionNumber], answerNumber, !!isChecked)),
+					answers: replaceArrayElement(answers, questionNumber, handleSelection(answers[questionNumber], answerNumber, !!isChecked)),
 				}
 			},
 		},
