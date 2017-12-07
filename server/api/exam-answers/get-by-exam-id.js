f4fe7dc44834fa2672aea0f303406da3f6e1939b
staskorz <staskorz@gmail.com>
2017-12-07 21:16:17 +0200

    Extract method that returns exam answers by exam ID into a dedicated module.

---
+++ server/api/exam-answers/get-by-exam-id.js
@@ -0,0 +1,68 @@
+import getAllUsersObject from '../utils/get-all-users-object'
+import getExamName from '../utils/get-exam-name'
+import onlyLast from '../utils/only-last'
+import sortByKey from '../utils/sort-by-key'
+
+export default (req, res) => {
+	const { db } = req
+	
+	const examId = req.params['examId']
+	
+	const examAnswersCollection = db.collection('Answers')
+	const examsCollection = db.collection('Exams')
+	const usersCollection = db.collection('users')
+	
+	const projection = {
+		fields: {
+			examineeUserId: 1,
+			examTimestamp: 1,
+			mark: 1,
+		},
+		
+		sort: {
+			examineeUserId: 1,
+			examTimestamp: 1,
+		},
+	}
+	
+	const getExistingUserDetails = ({ englishName, hebrewName, employeeId }) => ({
+		englishName,
+		hebrewName,
+		employeeId,
+	})
+	
+	const nonExistingUserDetails = {
+		englishName: '?',
+		hebrewName: '?',
+		employeeId: '?',
+	}
+	
+	const getUserDetails = (userId, allUsers) =>
+			userId && allUsers[userId] ? getExistingUserDetails(allUsers[userId]) : nonExistingUserDetails
+	
+	examAnswersCollection.find({ examId }, projection).toArray()
+			.then(value => onlyLast(value, 'examineeUserId'))
+			.then(value => Promise.all([
+				value,
+				getAllUsersObject(usersCollection),
+				getExamName(examsCollection, examId),
+			]))
+			.then(([lastExamAnswers, allUsers, examName]) => lastExamAnswers.map(({ examineeUserId, ...rest }) => ({
+				userId: examineeUserId,
+				username: allUsers[examineeUserId].username,
+				...rest,
+				...getUserDetails(examineeUserId, allUsers),
+				examId,
+				examName,
+			})))
+			.then(value => sortByKey(value, 'hebrewName'))
+			.then(value => {
+				res.json(value)
+			}).catch(err => {
+		const errorMessage = 'Error fetching exam answers from DB.'
+		
+		res.status(500).send(errorMessage)
+		
+		throw new Error(errorMessage + ' ' + err.name + ': ', err.message)
+	})
+}
--- server/api/exam-answers/index.js
+++ server/api/exam-answers/index.js
@@ -1,77 +1,12 @@
 import { Router } from 'express'
 
-import onlyLast from '../utils/only-last'
-import getAllUsersObject from '../utils/get-all-users-object'
-import getExamName from '../utils/get-exam-name'
-import sortByKey from '../utils/sort-by-key'
+import getByExamId from './get-by-exam-id'
 
 
 const router = Router()
 
 
-router.get('/:examId', (req, res) => {
-	const { db } = req
-	
-	const examId = req.params['examId']
-	
-	const examAnswersCollection = db.collection('Answers')
-	const examsCollection = db.collection('Exams')
-	const usersCollection = db.collection('users')
-	
-	const projection = {
-		fields: {
-			examineeUserId: 1,
-			examTimestamp: 1,
-			mark: 1,
-		},
-		
-		sort: {
-			examineeUserId: 1,
-			examTimestamp: 1,
-		},
-	}
-	
-	const getExistingUserDetails = ({ englishName, hebrewName, employeeId }) => ({
-		englishName,
-		hebrewName,
-		employeeId,
-	})
-	
-	const nonExistingUserDetails = {
-		englishName: '?',
-		hebrewName: '?',
-		employeeId: '?',
-	}
-	
-	const getUserDetails = (userId, allUsers) =>
-			userId && allUsers[userId] ? getExistingUserDetails(allUsers[userId]) : nonExistingUserDetails
-	
-	examAnswersCollection.find({ examId }, projection).toArray()
-			.then(value => onlyLast(value, 'examineeUserId'))
-			.then(value => Promise.all([
-				value,
-				getAllUsersObject(usersCollection),
-				getExamName(examsCollection, examId),
-			]))
-			.then(([lastExamAnswers, allUsers, examName]) => lastExamAnswers.map(({ examineeUserId, ...rest }) => ({
-				userId: examineeUserId,
-				username: allUsers[examineeUserId].username,
-				...rest,
-				...getUserDetails(examineeUserId, allUsers),
-				examId,
-				examName,
-			})))
-			.then(value => sortByKey(value, 'hebrewName'))
-			.then(value => {
-				res.json(value)
-			}).catch(err => {
-		const errorMessage = 'Error fetching exam answers from DB.'
-		
-		res.status(500).send(errorMessage)
-		
-		throw new Error(errorMessage + ' ' + err.name + ': ', err.message)
-	})
-})
+router.get('/:examId', getByExamId)
 
 
 export default router
