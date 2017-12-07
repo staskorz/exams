9623f07c7605ddacf4556dd48f8035c84d38a6f8
staskorz <staskorz@gmail.com>
2017-12-07 20:48:50 +0200

    Convert component to functional

--- imports/client/ui/pages/ExamTake/ExamMarkError.jsx
+++ imports/client/ui/pages/ExamTake/ExamMarkError.jsx
@@ -1,51 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Paper, RaisedButton } from 'material-ui'
-import { withRouter } from 'react-router'
 import { FormattedMessage } from 'react-intl'
 
 
-class ExamInfo extends Component {
-	style = {
-		mainPaper: {
-			padding: '32px',
-		},
-		
-		mainText: {
-			fontSize: '24px',
-			fontWeight: 'normal',
-			lineHeight: '36px',
-			fontFamily: 'Roboto, sans-serif',
-			color: 'rgba(0, 0, 0, 0.870588)',
-		},
-		
-		actions: {
-			paddingTop: '32px',
-		},
-		
-		button: {
-			marginRight: '16px',
-		},
-	}
+const style = {
+	mainPaper: {
+		padding: '32px',
+	},
 	
+	mainText: {
+		fontSize: '24px',
+		fontWeight: 'normal',
+		lineHeight: '36px',
+		fontFamily: 'Roboto, sans-serif',
+		color: 'rgba(0, 0, 0, 0.870588)',
+	},
 	
-	render() {
-		const { router, onSave } = this.props
-		
-		return <div className='main-container-padding'>
-			<Paper style={ this.style.mainPaper }>
-				<div>
-					<span style={ this.style.mainText }><FormattedMessage id='examMarkError' /></span>
-				</div>
-				
-				<div style={ this.style.actions }>
-					<RaisedButton label={ <FormattedMessage id='exit' /> } style={ this.style.button } onClick={ () => router.goBack() } />
-					
-					<RaisedButton label={ <FormattedMessage id='tryAgain' /> } style={ this.style.button } primary={ true } onClick={ onSave } />
-				</div>
-			</Paper>
-		</div>
-	}
+	actions: {
+		paddingTop: '32px',
+	},
+	
+	button: {
+		marginRight: '16px',
+	},
 }
 
 
-export default withRouter(ExamInfo)
+export default ({ router, onSave }) => <div className='main-container-padding'>
+	<Paper style={ style.mainPaper }>
+		<div>
+			<span style={ style.mainText }><FormattedMessage id='examMarkError' /></span>
+		</div>
+		
+		<div style={ style.actions }>
+			<RaisedButton label={ <FormattedMessage id='exit' /> } style={ style.button } onClick={ () => router.goBack() } />
+			
+			<RaisedButton label={ <FormattedMessage id='tryAgain' /> } style={ style.button } primary={ true } onClick={ onSave } />
+		</div>
+	</Paper>
+</div>
