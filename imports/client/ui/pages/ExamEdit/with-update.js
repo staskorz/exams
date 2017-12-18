c918bc989af9e972404104d063ef79f2896141ca
staskorz <staskorz@gmail.com>
2017-12-18 10:47:32 +0200

    Use rest.put instead of plain fetch

--- imports/client/ui/pages/ExamEdit/with-update.js
+++ imports/client/ui/pages/ExamEdit/with-update.js
@@ -1,20 +1,12 @@
 import { withHandlers } from 'recompose'
 
+import * as rest from '../../../rest'
+
 
 export default withHandlers({
 	onSave: ({ router, router: { params: { examId } } }) => value => {
-		fetch('/api/exams/' + examId, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(value),
-		}).then(({ ok }) => {
-			if(ok) {
-				router.push('/list-exams')
-			} else {
-				throw new Error('Cannot update exam')
-			}
+		rest.put('/api/exams/' + examId, value).then(() => {
+			router.push('/list-exams')
 		})
 	},
 })
--- imports/client/ui/pages/QuestionnaireEdit/with-update.js
+++ imports/client/ui/pages/QuestionnaireEdit/with-update.js
@@ -1,20 +1,12 @@
 import { withHandlers } from 'recompose'
 
+import * as rest from '../../../rest'
+
 
 export default withHandlers({
 	onSave: ({ value, router, router: { params: { questionnaireId } } }) => () => {
-		fetch('/api/questionnaires/' + questionnaireId, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(value),
-		}).then(({ ok }) => {
-			if(ok) {
-				router.push('/list-questionnaires')
-			} else {
-				throw new Error('Cannot update questionnaire')
-			}
+		rest.put('/api/questionnaires/' + questionnaireId, value).then(() => {
+			router.push('/list-questionnaires')
 		})
 	},
 })
