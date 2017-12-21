81a9ff254c7e2b405d7622089644f0011ad5276b
staskorz <staskorz@gmail.com>
2017-12-21 22:23:03 +0200

    Simplify Home component

--- client/pages/Home/Home.jsx
+++ client/pages/Home/Home.jsx
@@ -1,70 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card, CardTitle, CardText } from 'material-ui'
 
-import LoadingIndicator from '../../components/LoadingIndicator'
+
+const style = {
+	card: {
+		padding: '16px',
+	},
+	
+	cardText: {
+		minHeight: '250px',
+	},
+}
 
 
-export default class Home extends Component {
-	style = {
-		card: {
-			padding: '16px',
-		},
-		
-		cardText: {
-			minHeight: '250px',
-		},
-	}
+export default ({ currentUser: { username, englishName, hebrewName, employeeId } }) => {
+	const titleText = hebrewName ? hebrewName : (englishName ? englishName : username)
 	
+	const subTitleText = employeeId ? employeeId : ''
 	
-	prepareAnswersState = props => {
-		
-	}
-	
-	
-	componentWillMount() {
-		this.prepareAnswersState(this.props)
-	}
-	
-	
-	componentWillReceiveProps(nextProps) {
-		this.prepareAnswersState(nextProps)
-	}
-	
-	
-	render() {
-		const { currentUser } = this.props
-		
-		if(!currentUser || !currentUser.username) {
-			return <LoadingIndicator />
-		}
-		
-		const { username, englishName, hebrewName, employeeId } = currentUser
-		
-		let titleText
-		
-		if(hebrewName) {
-			titleText = hebrewName
-		} else if(englishName) {
-			titleText = englishName
-		} else {
-			titleText = username
-		}
-		
-		let subTitleText
-		
-		if(employeeId) {
-			subTitleText = employeeId
-		} else {
-			subTitleText = ''
-		}
-		
-		return <div className='main-container-padding'>
-			<Card style={ this.style.card }>
-				<CardTitle title={ titleText } subtitle={ subTitleText } />
-				
-				<CardText style={ this.style.cardText }>
-				</CardText>
-			</Card>
-		</div>
-	}
+	return <div className='main-container-padding'>
+		<Card style={ style.card }>
+			<CardTitle title={ titleText } subtitle={ subTitleText } />
+			
+			<CardText style={ style.cardText } />
+		</Card>
+	</div>
 }
