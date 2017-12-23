2d84069e3c1c8080e2f90d9b0615e0b53b7f37ca
staskorz <staskorz@gmail.com>
2017-12-23 21:26:11 +0200

    Set notification color to "neutral"

--- client/components/Notification/Notification.jsx
+++ client/components/Notification/Notification.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Snackbar } from 'material-ui'
 
+import { neutral } from '../../util/colors'
+
+
+const style = {
+	snackbarBody: {
+		backgroundColor: neutral,
+	},
+}
+
 
 export default ({ open, message, onRequestClose }) => <Snackbar
+		bodyStyle={ style.snackbarBody }
 		autoHideDuration={ 4000 }
 		open={ open }
 		onRequestClose={ onRequestClose }
