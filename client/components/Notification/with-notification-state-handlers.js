aae277a20c57101b96711ba9f15c54bf4bdbe1c7
staskorz <staskorz@gmail.com>
2017-12-23 21:12:51 +0200

    Notification now queues new messages if already open and displays them one by one

--- client/components/Notification/with-notification-state-handlers.js
+++ client/components/Notification/with-notification-state-handlers.js
@@ -4,14 +4,39 @@ import { withStateHandlers } from 'recompose'
 export default withStateHandlers({
 	message: '',
 	open: false,
+	queue: [],
 }, {
-	notify: () => message => ({
-		message,
-		open: true,
-	}),
+	notify: ({ open, queue }) => message => {
+		if(open) {
+			return {
+				queue: [ ...queue, message ],
+			}
+		} else {
+			return {
+				message,
+				open: true,
+			}
+		}
+	},
 	
-	onRequestClose: () => () => ({
-		message: '',
-		open: false,
-	}),
+	onRequestClose: ({ queue }) => reason => {
+		if(reason === 'clickaway') {
+			return
+		}
+		
+		if(queue.length > 0) {
+			const [ message, ...rest ] = queue
+			
+			return {
+				message,
+				open: true,
+				queue: rest,
+			}
+		} else {
+			return {
+				message: '',
+				open: false,
+			}
+		}
+	},
 })
