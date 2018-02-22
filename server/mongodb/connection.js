1dbcc390cc6c38f240cb5164c4c0bf363b57f7ad
staskorz <staskorz@gmail.com>
2018-02-22 11:53:44 +0200

    Allow getting MongoDB URL from environment variables

--- server/mongodb/connection.js
+++ server/mongodb/connection.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb'
 
 
-const MONGODB_URL = 'mongodb://localhost:27017'
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017'
 const DB_NAME = 'exams'
 
 
